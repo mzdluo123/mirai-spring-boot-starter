dd53a9d64ff9030a6887905a8be5c16dc62cf431
mzdluo123 <mzdluo123@foxmail.com>
2021-02-20 14:35:59 +0800

    修改依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ val mirai_version = "2.4.0"
 
 group = "io.github.mzdluo123"
 version = mirai_version
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
     mavenCentral()
@@ -22,7 +22,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    implementation("net.mamoe:mirai-core-jvm:$mirai_version")
+    implementation("net.mamoe:mirai-core:$mirai_version")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
