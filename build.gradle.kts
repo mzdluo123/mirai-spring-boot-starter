2100146e084e4a929b79ee98c24a9b7c5fde6b3d
mzdluo123 <mzdluo123@foxmail.com>
2021-02-20 14:58:54 +0800

    修改依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.3"
+//    id("org.springframework.boot") version "2.4.3" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.4.30"
     kotlin("plugin.spring") version "1.4.30"
+    id("maven")
 }
 
 val mirai_version = "2.4.0"
+val spring_version = "2.4.3"
 
 group = "io.github.mzdluo123"
 version = mirai_version
@@ -19,10 +21,17 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    implementation("net.mamoe:mirai-core:$mirai_version")
+    implementation("org.springframework.boot:spring-boot-starter:$spring_version")
+    implementation("org.springframework.boot:spring-boot-starter-validation:$spring_version")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:$spring_version")
+    implementation("net.mamoe:mirai-core-jvm:$mirai_version") {
+        exclude("net.mamoe","mirai-core-api")
+        exclude("net.mamoe","mirai-core-utils")
+    }
+    implementation("net.mamoe:mirai-core-api-jvm:$mirai_version") {
+        exclude("net.mamoe", "mirai-core-utils")
+    }
+    implementation("net.mamoe:mirai-core-utils-jvm:$mirai_version")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
