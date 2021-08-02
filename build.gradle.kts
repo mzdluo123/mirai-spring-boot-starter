f56117bd4bed7d93252de415b02a950ab48983e6
RainChan <mzdluo123@foxmail.com>
2021-08-02 13:41:26 +0800

    fix5

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     runtimeOnly("org.springframework.boot:spring-boot-starter:$spring_version")
     implementation("org.springframework.boot:spring-boot-starter-validation:$spring_version")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:$spring_version")
-    api("net.mamoe:mirai-core-jvm:$mirai_version")
+    runtimeOnly("net.mamoe:mirai-core-jvm:$mirai_version")
 //    implementation("org.jetbrains.kotlin:kotlin-reflect")
 //    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 //    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
@@ -34,6 +34,7 @@ tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "11"
+
     }
 }
 
